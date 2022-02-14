5270a304c896c7d7118d19244d9a972950073d27
francisco Alcaraz <fran.alcaraz@live.com>
2022-02-14 08:22:14 -0300

    clase01

--- clase01/script.js
+++ clase01/script.js
@@ -1,2 +1,15 @@
 'use strict'
 
+let nombre = prompt("ingrese su nombre");
+        let anioNac = parseInt(prompt("ingrese año nacimiento"));
+        let anioActual = parseInt(prompt("ingrese el año actual"));
+
+
+        let edad;
+
+        edad = anioActual - anioNac;
+        alert("Hola "+nombre + "\n su edad es de: " + edad);
+        console.log("Hola "+nombre + "\n su edad es de: " + edad);
+
+
+

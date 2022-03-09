0fbd668b992cac876dd67b9b5c95d64baef2f351
francisco Alcaraz <fran.alcaraz@live.com>
2022-03-09 12:30:26 -0300

    clase05.1

--- clase05/js/script.js
+++ clase05/js/script.js
@@ -1,75 +1,48 @@
 'use strict'
 
+
+class producto{
+    constructor(nombre, precio, stock, categoria){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock= stock;
+        this.categoria = categoria;
+    }
+};
+
 const productos = [ 
     {
         nombre:"pantalon Wara Marron",
         precio: 3200,
         stock:10,
-        categoria:"pantalon",
-        img :"https://i.ibb.co/RHkdMtM/webchicoana-11.jpg",
+        categoria:"pantalon"
     },
-    {
-        nombre:"pantalon Wara Crudo",
-        precio: 3200,
-        stock:10,
-        categoria:"pantalon",
-        img :"https://i.ibb.co/YXDF0gy/webchicoana-15.jpg",
-    },
+    
     {
         nombre:"Blusa Nina Verde",
         precio:1400,
         stock:14,
-        categoria:"blusa",
-        img :"https://i.ibb.co/HnJYmDM/webchicoana-46.jpg",
+        categoria:"blusa"
     },
-    {
-        nombre:"Blusa Nina Rosa",
-        precio:1400,
-        stock:14,
-        categoria:"blusa",
-        img :"https://i.ibb.co/0Fh2nzp/webchicoana-37.jpg",
-    },
+   
     {
         nombre:" Top Chari Blanco",
         precio:1000,
         stock:12,
-        categoria:"top",
-        img :"https://i.ibb.co/wQFFJJd/webchicoana-63.jpg",
+        categoria:"top"
     },
-    {
-        nombre:"Top Chari Celeste",
-        precio:1000,
-        stock:12,
-        categoria:"top",
-        img :"https://i.ibb.co/mtD1fVL/webchicoana-19.jpg",
-    },
+    
     {
         nombre:"Entero Amuk Verde",
         precio:2800,
         stock:22,
-        categoria:"entero",
-        img :"https://i.ibb.co/NNgNd3B/webchicoana.jpg",
+        categoria:"entero"
     },
-    {
-        nombre:"Entero Amuk Camel",
-        precio:2800,
-        stock:22,
-        categoria:"entero",
-        img :"https://i.ibb.co/grhNjfS/webchicoana-32.jpg",
-    },
-    {
-        nombre:"Vestido Samay Tostado",
-        precio: 2000,
-        stock:7,
-        categoria:"vestido",
-        img :"https://i.ibb.co/zS90FWB/webchicoana-30.jpg",
-    },
-    {
+   {
         nombre:"Vestido Samay Lila",
         precio: 2000,
         stock:7,
-        categoria:"vestido",
-        img :"https://i.ibb.co/RvqXxZZ/webchicoana-69.jpg",
+        categoria:"vestido"
     },
 ];
 
@@ -78,13 +51,29 @@ const productos = [
 
 function mostrarProductos (){
 
-productos.forEach((producto => {
-
+for (const producto of productos){
     console.log(producto.nombre);
     console.log(producto.precio);
-    
-}));
+}
+
+};
+function agregarProducto(nombre, precio, stock, categoria){
+    nombre = prompt("ingrese nombre");
+    precio =prompt.parseInt(("ingrese precio"));
+    stock = prompt("ingrese stock");
+    categoria =prompt("ingrese categoria");
+
+    productos.push(new producto(nombre, precio, stock, categoria));
+
+
 
 };
 
+
+productos.push(new producto("Pantalon Javi", 3000, 10, "pantalones"));
+
+
 mostrarProductos();
+agregarProducto();
+mostrarProductos();
+

0f96eceaa70b11d092185d8012e32d2ae9e69d40
Fran Alcaraz <fran.alcaraz@live.com>
2022-03-17 19:43:27 -0300

    1er ENTREGA FINAL

--- proyectoFinal/js/script.js
+++ proyectoFinal/js/script.js
@@ -15,13 +15,11 @@ class producto{
 
 function agregarProducto(nombre, precio, stock, categoria){
     nombre = prompt("ingrese nombre");
-    precio =prompt.parseInt(("ingrese precio"));
+    precio =parseInt(prompt("ingrese precio"));
     stock = prompt("ingrese stock");
     categoria =prompt("ingrese categoria");
 
     productos.push(new producto(nombre, precio, stock, categoria));
-
-
 
 };
 
@@ -29,18 +27,22 @@ function agregarProducto(nombre, precio, stock, categoria){
 
 productos.push(new producto("Pantalon Javi", 3000, 10, "pantalones"));
 productos.push(new producto("Remera Afrik", 3420, 11, "remeras"));
+productos.push(new producto("Remeron js", 2000, 5, "remeras"));
+agregarProducto();
+
 
 function ordenarPrecio(){
     var ordenadosPrecio = [];
     var index = 1;
     let stringOrdenado = "";
     ordenadosPrecio = productos.map(elemento => elemento);
-    var ordenadosPrecio = productos;
+ 
+ 
     ordenadosPrecio.sort(function(a,b){
         return a.precio - b.precio;
     });
     for(var elemento of ordenadosPrecio){
-        stringOrdenado = stringOrdenado + index + ": " + elemento.nombre + "\n" + elemento.precio + "\n";
+        stringOrdenado = stringOrdenado + index + ") Nombre: " + elemento.nombre + "\n Precio: " + elemento.precio + "\n";
         index++;
     }
     console.log("productos en orden ascendente por precio \n" + stringOrdenado);
@@ -68,8 +70,21 @@ function mostrarProductos (){
         prod.categoria.includes("pantalones"));
         console.log(pantalones);
 
+    
     }
 
+    function mostrarRemeras(){
+ 
+        const remeras = productos.filter ((prod) => 
+        prod.categoria.includes("remeras"));
+        console.log(remeras);
+
+    }
+
+
     mostrarPantalones();
+
+    mostrarRemeras();
+
 
     
